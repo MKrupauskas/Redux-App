1e79d4a1748938ee74299b5927d0dec021be9698
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-06-16 13:56:29 +0300

    create store and components

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Single extends React.Component {
+	render() {
+		return (
+			<div className="single-photo">
+        im single
+			</div>
+		)
+	}
+}
+
+export default Single
